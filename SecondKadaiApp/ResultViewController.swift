d7a60b0b6fa6e4bc464cf70509d571af31a571aa
kosuke.mizuma <sketsitake11@gmail.com>
2020-08-29 12:18:29 +0900

    更新

---
+++ SecondKadaiApp/ResultViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ResultViewController.swift
+//  SecondKadaiApp
+//
+//  Created by 水間皓介 on 2020/08/28.
+//  Copyright © 2020 kousuke.mizuma. All rights reserved.
+//
+
+import UIKit
+
+class ResultViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    /*
+    // MARK: - Navigation
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
